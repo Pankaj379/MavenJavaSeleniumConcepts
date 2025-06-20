f9db9a1d92db1705418c5561c80ac0634f85be44
pankaj379 <pankajkundal777@gmail.com>
2025-06-20 23:24:22 +0530

    First

---
+++ src/test/java/day23/CSS_Locator.java
@@ -0,0 +1,28 @@
+package day23;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CSS_Locator {
+
+	public static void main(String[] args) {
+		// CSS selector locator.
+		
+		WebDriver driver = new ChromeDriver();
+		// URL: 
+		driver.get("https://www.nopcommerce.com/");
+		
+		// Maximize window.
+		driver.manage().window().maximize();
+		
+		// tagid:
+		
+		driver.findElement(By.cssSelector(""));
+		
+		
+
+	}
+
+}
